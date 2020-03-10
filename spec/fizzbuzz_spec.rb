b8f9f121501732f504aa4d86c22f715020bfc2ea
Faye Carter <fayecarter333@gmail.com>
2020-03-10 14:27:12 +0000

    fizzbuzz now passes for multiples of 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,15 +1,23 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "Fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
   end
 
-  it 'returns "fizz" when passed a multiple of 3' do
-    expect(fizzbuzz(6)).to eq 'fizz'
-    expect(fizzbuzz(9)).to eq 'fizz'
-    expect(fizzbuzz(12)).to eq 'fizz'
-    expect(fizzbuzz(5)).not_to eq 'fizz'
-    expect(fizzbuzz(7)).not_to eq 'fizz'
+  it 'returns "Fizz" when passed a multiple of 3' do
+    expect(fizzbuzz(6)).to eq 'Fizz'
+    expect(fizzbuzz(9)).to eq 'Fizz'
+    expect(fizzbuzz(12)).to eq 'Fizz'
+    expect(fizzbuzz(8)).not_to eq 'Fizz'
+    expect(fizzbuzz(7)).not_to eq 'Fizz'
+  end
+
+  it 'returns "Buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+    expect(fizzbuzz(10)).to eq 'Buzz'
+    expect(fizzbuzz(20)).to eq 'Buzz'
+    expect(fizzbuzz(4)).not_to eq 'Buzz'
+    expect(fizzbuzz(7)).not_to eq 'Buzz'
   end
 end
